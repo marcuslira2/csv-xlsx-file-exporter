c6eed2871d10527cc792b682b47ef26fccb65706
Marcus Lira <marcus@usto.re>
2024-05-15 23:52:18 -0300

    first commit

---
+++ src/main/java/com/file/exporter/exporter/FileExporterApplication.java
@@ -0,0 +1,13 @@
+package com.file.exporter.exporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileExporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileExporterApplication.class, args);
+	}
+
+}
